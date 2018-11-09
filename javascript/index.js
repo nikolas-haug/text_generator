5199405aeb163bcdcb46bd4e828c06e8ade63760
Nikolas  Haug <nikolashaug@Nikolass-MacBook-Air.local>
2018-11-09 15:53:58 -0600

    add initial text function and listener

--- javascript/index.js
+++ javascript/index.js
@@ -4,3 +4,15 @@ const melvilleText = "Call me Ishmael. Some years ago—never mind how long prec
 
 const calvinoText = "From there, after six days and seven nights, you arrive at Zobeide, the white city, well exposed to the moon, with streets wound about themselves as in a skein. They tell this tale of its foundation: men of various nations had an identical dream. They saw a woman running at night through an unknown city; she was seen from behind, with long hair, and she was naked. They dreamed of pursuing her. As they twisted and turned, each of them lost her. After the dream, they set out in search of that city; they never found it, but they found one another; they decided to build a city like the one in the dream. In laying out the streets, each followed the course of his pursuit; at the spot where they had lost the fugitive's trail, they arranged spaces and walls differently from the dream, so she would be unable to escape again. This was the city of Zobeide, where they settled, waiting for that scene to be repeated one night. None of them, asleep or awake, ever saw the woman again. The city's streets were streets where they went to work every day, with no link any more to the dreamed chase. Which, for that matter, had long been forgotten. New men arrived from other lands, having had a dream like theirs, and in the city of Zobeide, they recognized something from the streets of the dream, and they changed the positions of arcades and stairways to resemble more closely the path of the pursued woman and so, at the spot where she had vanished, there would remain no avenue of escape. The first to arrive could not understand what drew these people to Zobeide, this ugly city, this trap."
 
+// make variables for buttons and text areas
+const addTextButton = document.querySelector('#add-text-btn');
+const textArea = document.querySelector('#main-text-area');
+
+// functions for event listeners
+function addText() {
+    // create the text node to add to text area from chosen text
+    chosenText = melvilleText.split(' ', 50).join(' ');
+    textArea.innerHTML = chosenText;
+}
+
+addTextButton.addEventListener('click', addText);
